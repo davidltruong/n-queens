6e4ed51cd14730ce602edfc83c7dec04725a32f5
Jackson Armstrong <jackson@Jacksons-MacBook-Pro.local>
2022-03-26 10:51:19 -0700

    Complete Bare Minimum Requirements

--- src/solvers.js
+++ src/solvers.js
@@ -98,54 +98,50 @@ window.findNQueensSolution = function(n) {
     debugger;
   }
 
-  var result;
   if (arguments[1]) {
     var solution = arguments[1];
     var row = arguments[2];
     var col = arguments[3];
-    var nPieces = arguments[4];
   } else {
     var solution = new Board({'n': n}); // [1, 0, 0]
     var row = 0;
     var col = 0;
-    var nPieces = 0;
   }
-  // if (row > n) {
-  //   return;
-  // }
-  if (nPieces === n) {
-    result = solution.rows();
-    return result;
+
+
+  var helper = function (matrix) {
+    var sum = 0;
+    for (var i = 0; i < matrix.length; i++) {
+      for (var j = 0; j < matrix[i].length; j++) {
+        sum += matrix[i][j];
+      }
+    }
+    return sum;
+  };
+
+  if (helper(solution.rows()) === n) {
+    return solution.rows();
   }
 
   for (row; row < n; row++) {
     for (col; col < n; col++) { //col = 2
       solution.togglePiece(row, col);
-      nPieces++;
-      row++;
       if (solution.hasAnyQueensConflicts()) {
-        row--;
         solution.togglePiece(row, col);
-        nPieces--;
       } else {
-        if (nPieces !== n) {
-          result = window.findNQueensSolution(n, solution, row, 0, nPieces);
-          if (nPieces === n) {
-            return result;
-          } else {
-            row--;
-            nPieces--;
-            solution.togglePiece(row, col);
-          }
+        row++;
+        window.findNQueensSolution(n, solution, row, 0);
+        if (helper(solution.rows()) === n) {
+          return solution.rows();
         } else {
-          result = window.findNQueensSolution(n, solution, row, 0, nPieces);
-          return result;
+          row--;
+          solution.togglePiece(row, col);
         }
       }
     }
   }
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return result;
+  // console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
